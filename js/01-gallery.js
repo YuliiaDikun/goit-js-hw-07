6d8b690d756abb5516ee8007d6424f0b92c1d139
Yuliia <dikun.yulia@gmail.com>
2022-10-23 10:51:21 +0300

    add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGallery = () => {
+   const gallery = galleryItems.map(item => {
+   const galleryItem =  `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+        </a>
+  </div>`;
+  return galleryItem;
+   });
+
+   galleryEl.innerHTML= gallery.join('');
+}
+createGallery();
