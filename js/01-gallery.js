b863da16d84da5e28223819434d0e49aa98c1f4b
Yuliia <dikun.yulia@gmail.com>
2022-10-23 22:23:40 +0300

    gallery fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector('.gallery');
 
-function createGallery () {
-   const gallery = galleryItems.map(item => {
-   const galleryItem = 
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-        <img
-            class="gallery__image"
-            src="${item.preview}"
-            data-source="${item.original}"
-            alt="${item.description}"
-        />
-        </a>
-    </div>`;
-  return galleryItem;
-   });
-
-   galleryEl.innerHTML= gallery.join('');
+function createGallery (images) {
+   return images.map(({original, preview, description}) => {
+    return`
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </div>`;
+  
+   })
+   .join('');
+   
 }
 
-createGallery();
+const imageMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
 
 function openGalleryModal (e) {
     e.preventDefault();
@@ -32,30 +32,13 @@ function openGalleryModal (e) {
         return;
     }  
     
-    const imgOnClick = e.target;
-    const src = imgOnClick.src;
-
-    const instance = basicLightbox.create(`${imgOnClick}`);
-        
-    instance.show();
-
-    const lightboxDiv = document.querySelector('.basicLightbox');
-    console.log(lightboxDiv);
-    console.log(lightboxDiv.classList.contains('basicLightbox--visible'));
-
-    if(lightboxDiv.classList.contains('basicLightbox--visible')) {
-            
-        imgOnClick.src = imgOnClick.dataset.source;
-        
-        
-        const lightboxPlaceholder = document.querySelector('.basicLightbox__placeholder');
-    
-        lightboxPlaceholder.style.width = '100vh';
-        lightboxPlaceholder.style.height = '100vh';
-        imgOnClick.style.width = '100vh';
-    
-        lightboxPlaceholder.append(imgOnClick);
-    }
+    const imgOnClick = e.target;   
+     
+    const src = imgOnClick.src;    
+     
+    const instance = basicLightbox.create(`<img src="${imgOnClick.dataset.source}" width="800" height="600">`);
+    console.log(instance);
+    instance.show();     
     
 }
 
