037000360f9fc25544420a312b1ad6576e6cac2e
Yuliia <dikun.yulia@gmail.com>
2022-10-23 23:58:35 +0300

    add simple lightbox task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createGallery (images) {
+   return images.map(({original, preview, description}) => {
+    return`
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+  
+   })
+   .join('');
+   
+}
+
+const imageMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a',  {
+    'spinner': false,
+    'close': false,
+    'showCounter': false,
+    'nav': false,
+});
+
+
