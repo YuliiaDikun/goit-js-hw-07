5911cd1d384ff53555c1807f4a0dbdab3840ea68
unknown <dikun.yulia@gmail.com>
2022-10-24 17:31:27 +0300

    fix libghtbox blinking

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('ul.gallery');
 
 function createGallery(images) {
   return images
     .map(({ original, preview, description }) => {
-      return `
-    <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>`;
+        return `
+      <div class="gallery">
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </div>`;
     })
     .join('');
 }
@@ -18,7 +20,7 @@ const imageMarkup = createGallery(galleryItems);
 
 galleryEl.insertAdjacentHTML('beforeend', imageMarkup);
 
-const lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('div.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
